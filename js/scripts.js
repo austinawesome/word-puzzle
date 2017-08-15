b35be93c59678fc99366483ad82249565b0559f7
Austin Cummings and Sarah Lindmar <sarah@>
2017-08-15 13:34:48 -0700

    make a form that takes a sentence and replaces the vowels with dashes to make a work puzzle

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  $("#form1").submit(function(event) {
+    var sentence = ($("#sentence").val());
+    var sentence2=[];
+    event.preventDefault();
+  for (i = 0; i <= sentence.length; i += 1) {
+      var pass = sentence[i];
+      if (pass != 'a' && pass !='e' && pass !='i' && pass !='o' && pass !='u') {
+      sentence2[i] = pass;
+    } else {
+      sentence2[i]='-';
+    }
+    if(i===sentence.length-1) {
+        $("#results").append("<p>" + sentence2.join('','') + "</p>");
+    }
+    };
+  });
+});
